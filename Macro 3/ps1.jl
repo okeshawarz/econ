aff79b38bc4da6ae6bb674704f58cf00f8256d09
Osman Keshawarz <osman.keshawarz@gmail.com>
2022-05-08 23:48:47 -0400

    more stuff

--- Macro 3/ps1.jl
+++ Macro 3/ps1.jl
@@ -1,12 +1,22 @@
 using Symbolics, DifferentialEquations, Plots, Latexify
-import SymPy
+import SymPy as sp
 
-### a
+x, y, z = sp.@vars x y z
+xdot = 2 * x - y
+ydot = x - y
 
-@variables t x y
-D = Differential(t)
-eqs = [0 ~ 2x - y,
-       0 ~ x - y]
+x_ss = sp.Eq(xdot, 0)
+y_ss = sp.Eq(ydot, 0)
+x_eq = sp.solve(x_ss, x)
+y_eq = sp.solve(y_ss, y)
 
-sol = Symbolics.solve_for(eqs, [x, y])
-latexify(sol)  |> render
+#Alternate: 
+
+rhs = [2x - y,
+       x - y]
+
+fps = sp.solve(rhs, [x, y])
+
+# Jacobian
+J = rhs.jacobian([x, y])
+
