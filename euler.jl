9adcba987eb02b3caa7b4cbaea3878a71e8bb3f5
Osman Keshawarz <17435367+okeshawarz@users.noreply.github.com>
2023-03-28 11:59:53 -0400

    problem 2

--- euler.jl
+++ euler.jl
@@ -4,3 +4,15 @@ sum(div)
 
 # Problem 2
 # find terms of fibonacci sequence less than 4,000,000
+
+function fib(x::Int)
+    vec = [1, 2]
+    while last(vec) <= x
+        append!(vec, (vec[end] + vec[end-1]))
+    end
+    return vec
+end
+v = fib(4000000)
+v = v[v .<= 4000000]
+v_even = v[v .% 2 .== 0] # select all elements for whom the modulus/2 is 0
+sum(v_even)
