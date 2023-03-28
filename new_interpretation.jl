a9593fe05135478addef2b0658aece0fb72c6006
Osman Keshawarz <17435367+okeshawarz@users.noreply.github.com>
2023-03-27 22:59:31 -0400

    it works now

--- new_interpretation.jl
+++ new_interpretation.jl
@@ -1,66 +1,47 @@
-using LinearAlgebra, Statistics, Plots, Latexify
-# This code replicates the example discussed in 
-# 7.A.4 of Basu(2021).
+using LinearAlgebra, Roots, Plots
 
-#### Quantities that are taken as given
-# A = input-output matrix
-# l = labour-input vector
-# w = nominal wage rate
-# v = value of labor power
-# Q = gross output vector
-
-A = [186/450 54/21 30/60;
-     12/450 6/21 3/60;
+# nxn IO matrix
+A = [186/450 54/21 30/60
+     12/450 6/21 3/60
      9/450 6/21 15/60]
 
+# 1xn direct labor input vector
 l = [18/450 12/21 30/60]
 
+# nominal wage rate
 w = 1
 
-v = 0.515
+# value of labor-power
+v = (1/3)
 
-y = [180 
-     0 
-     30]
+# nx1 net output vector
+y = [180; 0; 30;;]
 
-# Compute Gross Output
-x = (I-A)\y
+# Gross output
+Q = (I-A)\y
 
-# Maximum Eigenvalue of A
+# Maximum  eigenvalue of A
 λ_mA = eigmax(A)
 
 # Maximal rate of profit
 R = (1/λ_mA) - 1
 
-# Define Univariate function of the rate of profit
+# WORKS FINE UP UNTIL HERE
 
-f(r2) = (1 + r2)w*l*inv((I - (1+r2)A))*(I - A)*x - (w*l*x/v)
-h(r2) = (1 + r2)w*l*((I-1+r2)\(I-A)*x) - (w*l*x/v)
+# univariate function of r:
+# (This generates a 1x1 matrix so just take the first element)
 
-# Doesn't work, but it should. Alternate way:
-function g(r2)
-    D2 = (I-A)*x
-    l2 = l
-    A2 = A
-    L2 = (l*x)/v
-    C2 = (I-(1+r2)*A2)
-    E2 = C2\D2
-    B2 = (1+r2)*l2*E2
-    i2 = B2-L2
-    return(i2)
-end
+f(r2) = ((1+r2)*w*l*inv(I-(1+r2)A)*(I-A)Q - (w*l*Q/v))[1]
 
-using Roots
-r = find_zero(g, (0, R-0.1))
+r = find_zeros(f, (0, R-0.01))[1]
 
-#WARNING: Value for r given here different than in text. 
-#Python gives the same value-- maybe difference in root finding algorithm?
+#GREAT SUCCESSSSSSSSSSSSSSSS
 
-# Solve for prices of production
-p = (1+r)*w*l*(inv(I-(1+r)*A))
+# Vector of prices of production
+p = (1+r)*w*l*(inv(I-(1+r)A))
 
 # Vector of values
 Λ = l*inv(I-A)
 
-# MEV
-mev = (p*y')/(Λ*y')
+# MEV/MELT
+melt = (p*y)/(Λ*y)
